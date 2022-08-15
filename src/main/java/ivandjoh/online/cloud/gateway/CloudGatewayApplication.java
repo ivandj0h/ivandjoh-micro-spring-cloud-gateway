e11b47ec5deea92f269cc1aa48d0643a47c565e6
ivandjoh <ivandi.djoh@gmail.com>
2022-08-16 03:34:38 +0800

    downgrade spring to 2.6.10

--- src/main/java/ivandjoh/online/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/ivandjoh/online/cloud/gateway/CloudGatewayApplication.java
@@ -2,8 +2,10 @@ package ivandjoh.online.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
